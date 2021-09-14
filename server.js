3d6daef5ed0965970d1dfd90b9b9c4d611dcd7c1
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2021-09-14 18:14:52 -0500

    added some prompts for easy sections

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const consoleTable = require('console.table');
 //mysql2 connection folder
 require('./connection/mysql');
 
+//************Make 2 validations for number and string values */
+
 //Validation
 const valid = (input) => {
     if(input === '' || input === undefined) {
@@ -49,22 +51,47 @@ const roles = [
 //inquirer prompts
 const lobby = () => {
     console.log('Let\'s begin!')
-    inquirer.prompt([
+    inquirer.prompt(
         {
             type: 'list',
             name: 'centralLobby',
             message: 'What would you like to do?',
             choices: lobbyOptions
         }
-    ]).then((data) => { //All but quit go back to centralLobby
+    ).then((data) => { //All but quit go back to centralLobby
         if (data.centralLobby === 'View All Employees') {
             //display all employees everything
         } else if (data.centralLobby === 'Add Employee') {
-            //ask What is employees first name? input
-            //ask What is employees last name? input
-            //ask What is employees role? list
-            //ask Who is the employees Manager? list of empoloyee names(none if manager)
-            //add to database and success message
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'firstName',
+                    message: 'What is the employee\'s first name?',
+                    validate: valid
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message: 'What is the employee\'s last name?',
+                    validate: valid
+                },
+                {
+                    type: 'list',
+                    name: 'employeeRole',
+                    message: 'What is the employee\'s role?',
+                    choices: roles
+                },
+                {
+                    type: 'list',
+                    name: 'managers',
+                    message: 'Who is the employee\'s manager?',
+                    //need to add choices, maybe use a query, put into a variable, then take the values
+                    //need to add none to the list somehow as well
+                }
+            ]).then((data) => {
+                //add data to database table and give success message
+                lobby();
+            });
         } else if (data.centralLobby === 'Update Employee Role') {
             //ask Which employee's role would you like to update? list of employee names
             //ask Which role do you want to assign the selected employee? list
@@ -73,16 +100,50 @@ const lobby = () => {
             //display all roles
         } else if (data.centralLobby === 'Add role') {
             //ask What is the name of the role? input
-            //ask What is the salary of the role? list
-            //ask Which department it belongs to? list
-            //add to database and success message
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'roles',
+                    message: 'What is the name of the role?',
+                    validate: valid
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'What is the salary of the role?',
+                    validate: valid
+                },
+                {
+                    type: 'list',
+                    name: 'departments',
+                    message: 'Which department does this role belong to?',
+                    choices: departments
+                }
+            ]).then((data) => {
+                //add data to database table and give success message
+                lobby();
+            })
         } else if (data.centralLobby === 'View All Departments') {
             //display all departments
         } else if (data.centralLobby === 'Add Department') {
             //ask what is the name of the department
-            //then add to database and give success message
+            inquirer.prompt(
+                {
+                    type: 'input',
+                    name: 'newDepartment',
+                    message: 'What is the name of the department?',
+                    validate: valid
+                }
+            ).then((data) => {
+                //adds new department to the array of departments for list selection
+                let addDepartment = data.newDepartment;
+                departments.push(addDepartment)
+            })
         } else {
-            //quit
+            //quit, with goodbye message maybe
         }
     })
 }
+
+//invokes prompts
+lobby();
